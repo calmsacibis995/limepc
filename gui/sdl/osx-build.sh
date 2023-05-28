2b79c518aa350ecf50b4175eea668b8b6a4b0991
Stefanos Stefanidis <46319359+calmsacibis995@users.noreply.github.com>
2023-05-28 11:23:10 +0300

    Fix paths for build scripts.

--- gui/sdl/osx-build.sh
+++ gui/sdl/osx-build.sh
@@ -4,15 +4,15 @@ CC=gcc
 
 files=""
 for i in adb fpu mc68851 mem via floppy core_api cpu dis; do
-	perl ../core/macro.pl ../core/$i.c $i.post.c
+	perl ../../core/macro.pl ../../core/$i.c $i.post.c
 	files="$files $i.post.c"
 done
 
 for i in SoftFloat/softfloat atrap_tab coff exception macii_symbols redblack scsi video filesystem alloc_pool toby_frame_buffer ethernet sound; do
-	files="$files ../core/$i.c"
+	files="$files ../../core/$i.c"
 done
 
-$CC -O1 ../core/decoder_gen.c -o decoder_gen
+$CC -O1 ../../core/decoder_gen.c -o decoder_gen
 ./decoder_gen inst .
 ./decoder_gen dis .
 
